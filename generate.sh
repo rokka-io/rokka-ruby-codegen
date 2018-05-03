5f96deb7809fd449e97ffdee8803f4590779e81f
Christian Stocker <me@chregu.tv>
2018-05-03 16:57:33 +0200

    adjust readme

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 swagger-codegen generate -i https://api.rokka.io/doc.json -l ruby -o . -c config.json
-patch -p1 < patches/*.patch
+for i in patches/*.patch; do patch -p1 < $i; done
